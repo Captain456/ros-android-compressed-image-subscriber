1cb488d545ea432c93b3b1b919281050139134f1
Christopher Gibbs <Christopher_Gibbs@student.uml.edu>
2018-11-19 10:51:05 -0500

    Added ROS-specific things. App is currently broken in this state.

--- app/src/main/java/com/example/cgibbs/compressedimagesubscriber/ImageViewerActivity.java
+++ app/src/main/java/com/example/cgibbs/compressedimagesubscriber/ImageViewerActivity.java
@@ -4,8 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import org.ros.address.InetAddressFactory;
+import org.ros.android.BitmapFromCompressedImage;
+import org.ros.android.RosActivity;
+import org.ros.android.view.RosImageView;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
 
-public class ImageViewerActivity extends AppCompatActivity {
+import java.net.URI;
+
+public class ImageViewerActivity extends RosActivity {
+
+    /*
+     * Fields
+     */
+
+    private RosImageView<sensor_msgs.CompressedImage> image;
+
+    /*
+     * Constructors
+     */
+
+    public ImageViewerActivity() {
+        super("CompressedImagesSubscriber",
+                "CompressedImagesSubscriber",
+                URI.create("http://172.16.32.218:11311/"));
+    }
+
+    /*
+     * Protected Methods
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +45,18 @@ public class ImageViewerActivity extends AppCompatActivity {
         String url = intent.getStringExtra(MainActivity.EXTRA_URL);
         String topic = intent.getStringExtra(MainActivity.EXTRA_TOPIC);
 
-        // Set the textViews to the strings
-        TextView urlTextView = findViewById(R.id.textView3);
-        TextView topicTextView = findViewById(R.id.textView4);
+        // Subscribe to the image stream
+        image = (RosImageView<sensor_msgs.CompressedImage>) findViewById(R.id.image);
+        image.setTopicName(topic);
+        image.setMessageType(sensor_msgs.CompressedImage._TYPE);
+        image.setMessageToBitmapCallable(new BitmapFromCompressedImage());
+    }
 
-        urlTextView.setText(url);
-        topicTextView.setText(topic);
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+        NodeConfiguration nodeConfiguration =
+                NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(),
+                        getMasterUri());
+        nodeMainExecutor.execute(image, nodeConfiguration.setNodeName("android/image_viewer"));
     }
 }
